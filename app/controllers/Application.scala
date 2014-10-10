960d4ef4858ec07d4bd1d81668fe1d3a0af9bfbd
toshi <t.toshi.0412@gmail.com>
2014-10-10 15:21:31 +0900

    Define ServiceInfo of twitter

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,8 +2,29 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.oauth._
 
 object Application extends Controller {
+
+
+  private val twitterServiceInfo = {
+    val twitterRequestTokenURL = "https://api.twitter.com/oauth/request_token"
+    val twitterAccessTokenURL = "https://api.twitter.com/oauth/access_token"
+    val twitterAuthorizationURL = "https://api.twitter.com/oauth/authorize"
+    val twitterConsumerKey =
+      ConsumerKey(
+        Play.current.configuration.getString("twitter.consumerKey").get,
+        Play.current.configuration.getString("twitter.consumerSecret").get
+      )
+    ServiceInfo(
+      twitterRequestTokenURL,
+      twitterAccessTokenURL,
+      twitterAuthorizationURL,
+      twitterConsumerKey
+    )
+  }
+
+  private val oauth = OAuth(twitterServiceInfo, use10a = true)
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
